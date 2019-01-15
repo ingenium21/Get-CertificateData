cc70b5d1bc62dd19a754e0e19b2ffbdc82343eb7
Renato Regalado <renato.regalado@gmail.com>
2019-01-15 15:44:49 -0600

    Update getCertificateData.ps1

--- getCertificateData.ps1
+++ getCertificateData.ps1
@@ -1,4 +1,3 @@
-
 ##############################################
 # Author: Renato Regalado
 # Office: Mindshift Systems Engineer
@@ -10,57 +9,68 @@
 
 function get-CertificateData {
 
-[CmdletBinding()]
-Param (
-	[Parameter(Mandatory = $True, Position = 0)]
-	[string]$Ip,
-	[Parameter(Mandatory = $False, Position = 1)]
-	[string]$Port = "443"
-	)
+    [CmdletBinding()]
+    Param (
+	    [Parameter(Mandatory = $True, Position = 0)]
+	    [string]$Ip,
+	    [Parameter(Mandatory = $False, Position = 1)]
+	    [string]$Port = "443"
+	    )
 	
-#$Ip = "10.1.240.1"
-write-host 'working on $Ip'
-#$Port = 443
-$Connection = New-Object System.Net.Sockets.TcpClient($Ip,$Port)
-$Connection.SendTimeout = 5000
-$Connection.ReceiveTimeout = 5000
-$Stream = $Connection.GetStream()
+    #$Ip = "10.1.240.1"
+    #write-host "working on $Ip"
+    $testConnect = test-connection $Ip -Count 1
 
-try {
-    $sslStream = New-Object System.Net.Security.SslStream($Stream,$False,([Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}))
-    $sslStream.AuthenticateAsClient($null)
+    if($testConnect){
+        #$Port = 443
+        $nsname = (Resolve-DnsName $Ip).NameHost
+        $Connection = New-Object System.Net.Sockets.TcpClient($Ip,$Port)
+        $Connection.SendTimeout = 5000
+        $Connection.ReceiveTimeout = 5000
+        $Stream = $Connection.GetStream()
 
-    #$Certificate = [Security.Cryptography.X509Certificates.X509Certificate2]$sslStream.RemoteCertificate
+        try {
+            $sslStream = New-Object System.Net.Security.SslStream($Stream,$False,([Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}))
+            $sslStream.AuthenticateAsClient($null)
 
-    $cert = $sslStream.get_remotecertificate()
-    $cert2 = New-Object system.security.cryptography.x509certificates.x509certificate2($cert)
+            #$Certificate = [Security.Cryptography.X509Certificates.X509Certificate2]$sslStream.RemoteCertificate
 
-    $validto = [datetime]::Parse($cert.getexpirationdatestring())
-    $validfrom = [datetime]::Parse($cert.geteffectivedatestring())
+            $cert = $sslStream.get_remotecertificate()
+            $cert2 = New-Object system.security.cryptography.x509certificates.x509certificate2($cert)
 
-    if ($cert.get_issuer().CompareTo($cert.get_subject())) {
-        $selfsigned = "no";
-    } else {
-        $selfsigned = "yes";
-    }
+            $validto = [datetime]::Parse($cert.getexpirationdatestring())
+            $validfrom = [datetime]::Parse($cert.geteffectivedatestring())
 
-	$certObject = New-Object -TypeName PSObject -property ([Ordered] @{
-	'IP Address' = $Ip
-	'Port' = $Port
-	'Subject' = $cert.get_subject()
-	'Issuer' = $cert.get_issuer()
-	'Public Key' = $cert2.PublicKey.Key.KeySize
-	'Serial Number' = $cert.getserialnumberstring()
-	'Valid From' = $validfrom
-	'Valid to' = $validto
-	'Self Signed' = $selfsigned
-	'Signature Algorithm' = $cert2.SignatureAlgorithm.FriendlyName
-	})
+            if ($cert.get_issuer().CompareTo($cert.get_subject())) {
+                $selfsigned = "no";
+            } else {
+                $selfsigned = "yes";
+            }
+
+	        $certObject = New-Object -TypeName PSObject -property ([Ordered] @{
+            'Server Name from DNS' = $nsname
+	        'IP Address' = $Ip
+	        'Port' = $Port
+	        'Subject' = $cert.get_subject()
+	        'Issuer' = $cert.get_issuer()
+	        'Public Key' = $cert2.PublicKey.Key.KeySize
+	        'Serial Number' = $cert.getserialnumberstring()
+	        'Valid From' = $validfrom
+	        'Valid to' = $validto
+	        'Self Signed' = $selfsigned
+	        'Signature Algorithm' = $cert2.SignatureAlgorithm.FriendlyName
+	        })
 		
 
+        }
+        finally {
+            $Connection.Close()
+        }
+        return $certObject
+        }
+    else {
+        #write-host "Could not connect to $Ip"
+        }
 }
-finally {
-    $Connection.Close()
-}
-return $certObject
-}
+
+
