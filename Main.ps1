70893af6631a9dbf342e020bcc983f0d5de5d280
Renato Regalado <renato.regalado@gmail.com>
2019-06-03 16:06:16 -0500

    Added new functions

---
+++ Main.ps1
@@ -0,0 +1,16 @@
+﻿. .\Functions\getCertificateData.ps1
+
+$ipAddresses = get-content .\ipaddresses.txt
+$resultsPath = ".\results.csv"
+
+if (!(Test-Path $resultsPath)) {
+    new-item -type file .\results.csv
+}
+
+foreach ($ip in $ipAddresses) {
+    $results = get-CertificateData -Ip $ip -ErrorAction SilentlyContinue
+     
+    if ($results -ne $null){
+        $results | export-csv .\results.csv -Append
+    }
+}
