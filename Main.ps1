722d622dad3d9dbad485f860174828d10739ce72
Renato Regalado <renato.regalado@gmail.com>
2019-06-05 12:38:24 -0500

    added several new things
    -added the get-IPRange function
    -added conditionals for different parameters
    -changed if conditional to ensure $results aren't null

--- Main.ps1
+++ Main.ps1
@@ -1,6 +1,36 @@
 ﻿. .\Functions\getCertificateData.ps1
+. .\Functions\get-IPRange.ps1
 
-$ipAddresses = get-content .\ipaddresses.txt
+param (
+    [Parameter(Mandatory=$true)][string]$ipAddress,
+    [string]$start,
+    [string]$end,
+    [string]$mask,
+    [int]$cidr
+)
+
+$ipAddresses = @()
+
+if ($ipAddress) {
+    $ipAddresses = $ipAddress
+}
+
+elseif ($start -and $end) {
+    $ipAddresses = get-IPRange -start $start -end $end
+}
+
+elseif ($ipAddress -and $mask) {
+    $ipAddresses = get-IPRange -ip $ipAddress -mask $mask
+}
+
+elseif ($ipAddress -and $cidr) {
+    $ipAddresses = get-IPRange -ip $ipAddress -cidr $cidr
+}
+
+else {
+    Write-host -ForegroundColor Red "No valid IP information was input"
+}
+
 $resultsPath = ".\results.csv"
 
 if (!(Test-Path $resultsPath)) {
@@ -10,7 +40,7 @@ if (!(Test-Path $resultsPath)) {
 foreach ($ip in $ipAddresses) {
     $results = get-CertificateData -Ip $ip -ErrorAction SilentlyContinue
      
-    if ($results -ne $null){
+    if ($null -ne $results){
         $results | export-csv .\results.csv -Append
     }
 }
